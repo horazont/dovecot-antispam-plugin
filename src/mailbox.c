90612d1043ea3feaef1adea0f608a118ab52d1b5
Eugene Paskevich <eugene@raptor.kiev.ua>
2012-02-26 21:34:05 +0200

    Renamed copying to copying_via_save.

--- src/mailbox.c
+++ src/mailbox.c
@@ -135,7 +135,7 @@ static int antispam_save_begin(struct mail_save_context *ctx,
     struct mailbox_transaction_context *t = ctx->transaction;
     struct antispam_mailbox *asmb = STORAGE_CONTEXT(t->box);
 
-    if (ctx->copying == 0)
+    if (ctx->copying_via_save == 0)
     {
 	struct antispam_user *asu = USER_CONTEXT(t->box->storage->user);
 
@@ -171,7 +171,7 @@ static int antispam_save_finish(struct mail_save_context *ctx)
 
     // if we are copying then copy() code will do everything needed
     int ret = asmb->module_ctx.super.save_finish(ctx);
-    if (ctx->copying != 0 || ret != 0)
+    if (ctx->copying_via_save != 0 || ret != 0)
 	return ret;
 
     // since there is no source mailbox, let's assume
